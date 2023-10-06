5c2c513bb912288a1ba6e74789f04833eb2985a7
Habbatul <gamernolifeneet@gmail.com>
2023-10-06 21:27:05 +0700

    Done #1

--- src/test/java/com/habbatul/challange4/ProductServiceTest.java
+++ src/test/java/com/habbatul/challange4/ProductServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
